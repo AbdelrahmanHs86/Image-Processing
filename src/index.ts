a5a91737879257ddfcb1b508ad4f0fd53856a67e
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-01 06:12:42 +0300

    feat: Start a server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Typescript is ready');
+import express from 'express';
+
+const app = express();
+const port = 3024;
+
+app.get('/', (req, res) => {
+	res.send('Perfect👌🏻');
+});
+
+app.listen(port, () => {
+	console.log(`Server Working at localhost:${port}`);
+});
