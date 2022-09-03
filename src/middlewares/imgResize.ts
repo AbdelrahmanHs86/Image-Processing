e1c8f9c09aa217025a33a0c4afa190b6dc51ed71
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-03 22:17:48 +0300

    refactor: Move imgResizer to Middleware folder

--- src/middlewares/imgResize.ts
+++ src/middlewares/imgResize.ts
@@ -1,17 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import sharp from 'sharp';
 import path from 'path';
-import { JSDOM } from 'jsdom';
-import isExist from './isExist';
+import isExist from '../modules/isExist';
 
 const imgResize = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 	try {
 		const Filename = req.query.filename as string;
 		const imgWidth = parseInt(req.query.width as string, 10);
 		const imgHeight = parseInt(req.query.height as string, 10);
-		const imgInput = path.join(__dirname, `./Images/Original/${Filename}.jpg`);
-		const imgOutput = path.join(__dirname, `./Images/Resized/img1_${imgWidth}_${imgHeight}.jpg`);
-
+		const imgInput = path.join(__dirname, `../Images/Original/${Filename}.jpg`);
+		const imgOutput = path.join(__dirname, `../Images/Resized/${Filename}_${imgWidth}_${imgHeight}.jpg`);
 		const exist = await isExist(imgOutput);
 		if (!exist) {
 			if (!Number.isNaN(imgWidth)) {
@@ -19,19 +17,17 @@ const imgResize = async (req: Request, res: Response, next: NextFunction): Promi
 					.resize(imgWidth, imgHeight)
 					.jpeg({ mozjpeg: true })
 					.toFile(imgOutput)
+					// eslint-disable-next-line no-console
 					.catch((err) => console.log('error: ', err));
-				// res.attachment(imgOutput);
-				res.set('Content-Type', 'text/html');
-				res.send(Buffer.from(`<p>some html <img src='${imgOutput}' ></p>`));
-				// res.send('<p>Done, Check Your resized Image</p>');
+				setTimeout(() => {
+					res.sendFile(imgOutput);
+				}, 1000);
 			}
 		} else {
-			// res.attachment(imgOutput);
-			res.send(`<p>Image Already has been resized <img src='${imgOutput}' ></p>`);
+			res.sendFile(imgOutput);
 		}
 	} catch (err) {
 		res.send('Something went Wrong! Check your inputs');
-		console.log('error: ', err);
 	}
 
 	next();
