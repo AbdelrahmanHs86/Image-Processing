60199b949ea2857853463a18c1944054f428a9a9
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-03 22:23:06 +0300

    feat: add html form for data entry

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ const app = express();
 const port = 3024;
 
 app.use('/api', routes, (req, res) => {
-	res.send('Image Rezizer API');
+	res.send(`
+	<form action="/api/resize">
+	<label for="fname">File name:</label><br>
+	<input type="text" id="filename" name="filename" value=""><br>
+	<label for="width">Image width:</label><br>
+	<input type="number" id="width" name="width" value=""><br>
+	<label for="height">Image height:</label><br>
+	<input type="number" id="height" name="height" value=""><br>
+	<br>
+	<input type="submit" value="Resize">
+   </form>
+`);
 });
 
 app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Server Working at localhost:${port}`);
 });
+
+export default app;
