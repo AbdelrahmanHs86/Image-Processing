c699c0c4bd121aa7533bf20f1d7cb0247f9ab2cd
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-05 04:06:34 +0300

    fix: Add a return type to the function

--- src/modules/isExist.ts
+++ src/modules/isExist.ts
@@ -1,6 +1,6 @@
 import { promises as fsPromises } from 'fs';
 
-const isExist = async (path: string) => {
+const isExist = async (path: string): Promise<boolean> => {
 	try {
 		await fsPromises.access(path);
 		return true;
